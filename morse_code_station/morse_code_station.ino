15964a908e94965b2c09acc35132b614b2ee5c2f
grahowe <omgraham02@gmail.com>
2023-09-26 13:23:21 -0500

    Renamed serial string
    You can customize this string to match your callsign, name, etc. I had KE0SBX, as this is my callsign

--- morse_code_station/morse_code_station.ino
+++ morse_code_station/morse_code_station.ino
@@ -64,7 +64,7 @@ void setup() {
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setCursor(0, 0);     // Start at top-left corner
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
-  display.write("KE0SBX CW Station");
+  display.write("Morse Code Station");
   display.drawLine(0, 10, display.width(), 10, SSD1306_WHITE);
 
   display.setCursor(0, 12);
